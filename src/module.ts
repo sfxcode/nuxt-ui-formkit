941f82bacddd279721a8e1d9424dd49d0374eda9
sfxcode <tom@sfxcode.com>
2026-02-21 19:27:44 +0100

    feat: extend FormKitInputProps with additional Nuxt UI input and output components

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,5 @@
 import { defineNuxtModule, addPlugin, createResolver, addComponent, addImportsDir } from '@nuxt/kit'
+import type { FormKitInputs } from '@formkit/inputs'
 
 // Module options TypeScript interface definition
 export type ModuleOptions = Record<string, never>
@@ -33,3 +34,76 @@ export default defineNuxtModule<ModuleOptions>({
     )
   },
 })
+
+declare module '@formkit/inputs' {
+  interface FormKitInputProps<Props extends FormKitInputs<Props>> {
+    // Input Components
+    nuxtUICheckbox: {
+      type: 'nuxtUICheckbox'
+    }
+    nuxtUICheckboxGroup: {
+      type: 'nuxtUICheckboxGroup'
+    }
+    nuxtUIColorPicker: {
+      type: 'nuxtUIColorPicker'
+    }
+    nuxtUIInput: {
+      type: 'nuxtUIInput'
+    }
+    nuxtUIInputDate: {
+      type: 'nuxtUIInputDate'
+    }
+    nuxtUIInputMenu: {
+      type: 'nuxtUIInputMenu'
+    }
+    nuxtUIInputNumber: {
+      type: 'nuxtUIInputNumber'
+    }
+    nuxtUIInputTags: {
+      type: 'nuxtUIInputTags'
+    }
+    nuxtUIInputTime: {
+      type: 'nuxtUIInputTime'
+    }
+    nuxtUIPinInput: {
+      type: 'nuxtUIPinInput'
+    }
+    nuxtUIRadioGroup: {
+      type: 'nuxtUIRadioGroup'
+    }
+    nuxtUISelect: {
+      type: 'nuxtUISelect'
+    }
+    nuxtUISelectMenu: {
+      type: 'nuxtUISelectMenu'
+    }
+    nuxtUISlider: {
+      type: 'nuxtUISlider'
+    }
+    nuxtUISwitch: {
+      type: 'nuxtUISwitch'
+    }
+    nuxtUITextarea: {
+      type: 'nuxtUITextarea'
+    }
+    // Output Components
+    nuxtUIOutputText: {
+      type: 'nuxtUIOutputText'
+    }
+    nuxtUIOutputBoolean: {
+      type: 'nuxtUIOutputBoolean'
+    }
+    nuxtUIOutputDate: {
+      type: 'nuxtUIOutputDate'
+    }
+    nuxtUIOutputLink: {
+      type: 'nuxtUIOutputLink'
+    }
+    nuxtUIOutputNumber: {
+      type: 'nuxtUIOutputNumber'
+    }
+    nuxtUIOutputList: {
+      type: 'nuxtUIOutputList'
+    }
+  }
+}
