2dbe95ea7062aab1f472914a2ff7de05353786c2
sfxcode <tom@sfxcode.com>
2026-02-22 22:24:07 +0100

    refactor: wrap content in client-only component for improved rendering

--- src/runtime/components/FUDataDebug.vue
+++ src/runtime/components/FUDataDebug.vue
@@ -17,15 +17,17 @@ defineProps({
 
 <template>
   <div class="formkit-debug">
-    <h3 class="text-lg font-semibold mb-2">
-      {{ header }}
-    </h3>
-    <slot />
-    <pre
-      v-if="data"
-      :class="textClass"
-    >{{ data }}</pre>
-    <span v-else>No Data available</span>
+    <client-only>
+      <h3 class="text-lg font-semibold mb-2">
+        {{ header }}
+      </h3>
+      <slot />
+      <pre
+        v-if="data"
+        :class="textClass"
+      >{{ data }}</pre>
+      <span v-else>No Data available</span>
+    </client-only>
   </div>
 </template>
 
