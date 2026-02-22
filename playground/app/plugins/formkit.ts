4134b200a71f97aede92b80740218dc6bbb274dd
sfxcode <tom@sfxcode.com>
2026-02-23 00:50:07 +0100

    feat: add UButton component for use in FormKit repeater

---
+++ playground/app/plugins/formkit.ts
@@ -0,0 +1,8 @@
+import { defineNuxtPlugin } from '#app'
+import { UButton } from '#components'
+
+export default defineNuxtPlugin((_nuxtApp) => {
+  // add Button for repeater
+
+  _nuxtApp.vueApp.component('UButton', UButton)
+})
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,7 +1,5 @@
 import { defineNuxtPlugin } from '#app'
-import { UButton } from '#components'
 
 export default defineNuxtPlugin((_nuxtApp) => {
-  // add Button for repeater
-  _nuxtApp.vueApp.component('UButton', UButton)
+
 })
