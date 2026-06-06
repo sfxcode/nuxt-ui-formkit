1ed3a4061821fd6cd0f4ca0f366bd7ce326c73b4
sfxcode <tom@sfxcode.com>
2026-06-06 12:31:13 +0200

    fix: improve button component logic with dynamic disabled states and enhance array handling in repeater

--- src/runtime/formkit/definitions/repeater.ts
+++ src/runtime/formkit/definitions/repeater.ts
@@ -5,22 +5,22 @@ import { useFormKitSchema } from '../../composables/useFormKitSchema'
 const { addList, addElement, addListGroup, addComponent } = useFormKitSchema()
 
 function addButtonGroup(buttonGroupClass: string = '', buttonGroupItemClass: string = '', buttonSize: string, render: string = 'true') {
-  const addActionButtonComponent = (onClick: string = '', icon: string = '', color: string = '', render: string = 'true', disabled: 'false'): object => {
+  const addActionButtonComponent = (onClick: string = '', icon: string = '', color: string = '', render: string = 'true', disabled: string = 'false'): object => {
     return addElement('div', [addComponent('UButton', { onClick, icon, color, disabled, size: buttonSize })], { class: buttonGroupItemClass }, render)
   }
 
   return addElement('div', [
     addActionButtonComponent('$moveNodeUp($node.parent, $index)', 'i-lucide-arrow-up', 'primary', '$renderMoveButtons', '$index === 0'),
-    addActionButtonComponent('$removeNode($node.parent, $index)', 'i-lucide-trash', 'error', '$displayDeleteButton'),
-    addActionButtonComponent('$cloneNode($node.parent, $index)', 'i-lucide-copy', 'secondary', '$displayCloneButton'),
-    addActionButtonComponent('$addNode($node.parent, $index)', 'i-lucide-plus', 'secondary', '$displayAddButton'),
+    addActionButtonComponent('$removeNode($node.parent, $index)', 'i-lucide-trash', 'error', '$displayDeleteButton', '$node.parent.value.length === $minItems'),
+    addActionButtonComponent('$cloneNode($node.parent, $index)', 'i-lucide-copy', 'secondary', '$displayCloneButton', '$node.parent.value.length > $maxItems -1'),
+    addActionButtonComponent('$addNode($node.parent, $index)', 'i-lucide-plus', 'secondary', '$displayAddButton', '$node.parent.value.length > $maxItems -1'),
     addActionButtonComponent('$moveNodeDown($node.parent, $index)', 'i-lucide-arrow-down', 'primary', '$renderMoveButtons', '$index === $node.parent.value.length -1'),
   ], { class: buttonGroupClass }, render)
 }
 
 function addInsertButton(label: string = 'Add Item', icon: string = 'i-lucide-plus', styleClass: string = '', buttonSize: string, render: string = 'true') {
   return addElement('div', [
-    addComponent('UButton', { onClick: '$insertNode($node)', label, icon, size: buttonSize }),
+    addComponent('UButton', { onClick: '$insertNode($node)', label, icon, size: buttonSize, disabled: '$node.value.length > $maxItems -1' }),
   ], { class: styleClass }, render)
 }
 
@@ -35,19 +35,21 @@ export const nuxtUIRepeaterDefinition: FormKitTypeDefinition = createInput(
   ], { class: '$internalListClass' }, true),
   {
     props: ['insertButtonLabel', 'insertButtonClass', 'insertButtonSize', 'alwaysDisplayInsertButton', 'newItem', 'listClass', 'listItemClass',
-      'hideButtonGroup', 'hideMoveButtons', 'buttonGroupClass', 'buttonGroupItemClass', 'buttonSize', 'displayCloneButton', 'displayAddButton', 'displayDeleteButton'],
+      'hideButtonGroup', 'hideMoveButtons', 'buttonGroupClass', 'buttonGroupItemClass', 'buttonSize', 'displayCloneButton', 'displayAddButton', 'displayDeleteButton',
+      'minItems', 'maxItems'],
     features: [addRepeaterHandler],
   },
 )
 
 function addRepeaterHandler(node: FormKitNode): void {
-  const swapElements = (array: object[], index1: number, index2: number) => {
-    const newArray = [...array]
-    const temp = newArray[index1]
-    newArray[index1] = newArray[index2]
-    newArray[index2] = temp
-    return newArray
+  const swapElements = <T>(array: T[], index1: number, index2: number): T[] => {
+    const [element] = array.splice(index1, 1)
+    if (element !== undefined) {
+      array.splice(index2, 0, element)
+    }
+    return array
   }
+
   node.on('created', () => {
     node.context.listName = node.name
     node.context.renderButtons = !node.context.hideButtonGroup
@@ -56,45 +58,48 @@ function addRepeaterHandler(node: FormKitNode): void {
     node.context.renderMoveButtons = !node.context.hideMoveButtons
     node.context.internalListClass = node.context.listClass ? `formkit-items ${node.context.listClass}` : 'formkit-items'
     node.context.internalListItemClass = node.context.listItemClass ? `formkit-item ${node.context.listItemClass}` : 'formkit-item'
+    node.context.minItems = node.context.minItems !== undefined ? node.context.minItems : 0
+    node.context.maxItems = node.context.maxItems !== undefined ? node.context.maxItems : Number.POSITIVE_INFINITY
 
     node.context.insertNode = (parentNode: FormKitNode) => (): void => {
-      if (parentNode && parentNode._value instanceof Array) {
+      if (parentNode && Array.isArray(parentNode._value)) {
         const item: object = node.context.newItem ? { ...node.context.newItem } : {}
-        const newArray: object[] = [item, ...parentNode._value]
-        parentNode.input(newArray, false)
+        const array: object[] = parentNode.value
+        array.push(item)
+        parentNode.input(array, false)
       }
     }
     node.context.removeNode = (parentNode: FormKitNode, index: number) => (): void => {
-      if (parentNode && parentNode._value instanceof Array) {
+      if (parentNode && Array.isArray(parentNode._value)) {
         parentNode.input(parentNode._value.filter((_: object, i: number): boolean => i !== index), false)
       }
     }
     node.context.addNode = (parentNode: FormKitNode, index: number) => (): void => {
-      if (parentNode && parentNode._value instanceof Array) {
+      if (parentNode && Array.isArray(parentNode._value)) {
+        const array: object[] = parentNode.value
         const item: object = node.context.newItem ? { ...node.context.newItem } : {}
-        const newArray: object[] = [...parentNode._value.slice(0, index + 1), { ...item }, ...parentNode._value.slice(index + 1)]
-        parentNode.input([...newArray], false)
+        array.splice(index + 1, 0, item)
+        parentNode.input(array, false)
       }
     }
     node.context.cloneNode = (parentNode: FormKitNode, index: number) => (): void => {
-      if (parentNode && parentNode._value instanceof Array) {
-        const item: object = parentNode._value[index]
-        const newArray: object[] = [...parentNode._value.slice(0, index + 1), { ...item }, ...parentNode._value.slice(index + 1)]
-        parentNode.input([...newArray], false)
+      if (parentNode && Array.isArray(parentNode._value)) {
+        const item: object = parentNode.value[index]
+        const array: object[] = parentNode.value
+        array.splice(index + 1, 0, item)
+        parentNode.input(array, false)
       }
     }
     node.context.moveNodeUp = (parentNode: FormKitNode, index: number) => (): void => {
-      if (parentNode && parentNode._value instanceof Array) {
-        const array: object[] = [...parentNode._value]
+      if (parentNode && Array.isArray(parentNode._value)) {
         if (index > 0)
-          parentNode.input(swapElements(array, index - 1, index), false)
+          parentNode.input(swapElements(parentNode.value, index - 1, index), false)
       }
     }
     node.context.moveNodeDown = (parentNode: FormKitNode, index: number) => (): void => {
-      if (parentNode && parentNode._value instanceof Array) {
-        const array: object[] = [...parentNode._value]
-        if (index < array.length - 1)
-          parentNode.input(swapElements(array, index, index + 1), false)
+      if (parentNode && Array.isArray(parentNode._value)) {
+        if (index < parentNode.value.length - 1)
+          parentNode.input(swapElements(parentNode.value, index, index + 1), false)
       }
     }
   })
