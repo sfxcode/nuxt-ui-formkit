3a88cf642cfef9f08d56ab53c247e269e69d8618
sfxcode <tom@sfxcode.com>
2026-08-04 22:38:41 +0200

    feat: add tooltip support to FUIcon component

--- src/runtime/components/output/FUIcon.vue
+++ src/runtime/components/output/FUIcon.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import type { PropType } from 'vue'
+import type { TooltipProps } from '#ui/components/Tooltip.vue'
 
 export interface FormKitIconProps {
   onIconClicked?: () => void
   onLeadingIconClicked?: () => void
   onTrailingIconClicked?: () => void
 }
+
+// `class` is excluded - icon sizing/coloring is already handled via the
+// `iconClass`/fallthrough-`class` mechanism below, not the tooltip wrapper.
+export type FUIconTooltip = Omit<TooltipProps, 'class'>
+
+defineOptions({ inheritAttrs: false })
 
 defineProps({
   name: {
@@ -17,12 +24,28 @@ defineProps({
     type: String,
     default: '',
   },
+  tooltip: {
+    type: Object as PropType<FUIconTooltip>,
+    default: undefined,
+  },
 })
 </script>
 
 <template>
+  <UTooltip
+    v-if="tooltip"
+    v-bind="tooltip"
+  >
+    <UIcon
+      :name="name"
+      v-bind="$attrs"
+      @click="onClick"
+    />
+  </UTooltip>
   <UIcon
+    v-else
     :name="name"
+    v-bind="$attrs"
     @click="onClick"
   />
 </template>
